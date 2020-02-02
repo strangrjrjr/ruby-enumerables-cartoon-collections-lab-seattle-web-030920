851c07a8cbc2f39f76acfd90f15aa4d0481f27cd
Roger Robertson Jr <rjrjr@pm.me>
2020-02-02 19:22:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  planeteer_calls.collect {
+  planeteer_calls.collect { |call| call.
 end
 
 def long_planeteer_calls# code an argument here
