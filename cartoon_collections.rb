7d00033229aa9b1916ee896bbe17422d8a938bad
Roger Robertson Jr <rjrjr@pm.me>
2020-02-02 19:16:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  
+  dwarves.each do |dwarf|
+    puts dwarf
+  end
 end
 
 def summon_captain_planet# code an argument here
