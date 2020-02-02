61ea588ace26f4194decf50909d1d6ecb5cc0bbf
Roger Robertson Jr <rjrjr@pm.me>
2020-02-02 19:21:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,9 @@ def roll_call_dwarves(dwarves)# code an argument here
   }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  
 end
 
 def long_planeteer_calls# code an argument here
