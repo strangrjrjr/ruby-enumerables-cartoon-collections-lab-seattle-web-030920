5170890569cffa240b3bed0531af23be11d29298
Roger Robertson Jr <rjrjr@pm.me>
2020-02-02 19:33:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,5 +25,5 @@ end
 def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  ingredients.include?(cheese_types)
 end
